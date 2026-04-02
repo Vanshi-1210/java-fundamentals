778436eefcba7449912b47a01d1466d2452272f8
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-04-02 16:58:11 +0530

    Removing next line

--- SumOfTwoNumbers.java
+++ SumOfTwoNumbers.java
@@ -4,9 +4,9 @@ public class SumOfTwoNumbers {
 
     public static void main(String[] args) {
         Scanner sc=new Scanner (System.in);
-       System.out.println("Enter the value of a:");
+       System.out.print("Enter the value of a:");
        int a=sc.nextInt();
-       System.out.println("Enter the value of b:");
+       System.out.print("Enter the value of b:");
        int b=sc.nextInt();
        int sum=a+b;
        System.out.println("Sum of a and b is:"+sum);
