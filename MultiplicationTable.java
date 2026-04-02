e4316146557d94116ed95e30e911409aa5af884b
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-04-03 00:07:10 +0530

    Implemented multiplication table using for and while loops with user

---
+++ MultiplicationTable.java
@@ -0,0 +1,23 @@
+// Loops Practice
+// Program to take a number as input and print its multiplication table
+// using both for loop and while loop
+import java.util.*;
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter your num:");
+        int num=sc.nextInt();
+        // using for loop-
+        // for(int i=1;i<=10;i++){
+        //     System.out.println(num*i);
+        // }
+        // using while loop
+        int i=1;
+        while(i<=10){
+            System.out.println(num*i);
+            i++;
+        }
+        sc.close();
+    }
+}
