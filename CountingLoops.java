9ccf1f7eca8c360b5c40a8562630b3c049629a0c
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-04-02 23:58:18 +0530

    Implemented counting from 0 to 10 using for and while loops

---
+++ CountingLoops.java
@@ -0,0 +1,19 @@
+// Loops Practice
+// Program to print counting from 0 to 10 using for loop and while loop.
+
+public class CountingLoops {
+
+    public static void main(String[] args) {
+        // using for loop-
+        for(int i=0;i<=10;i++){
+            System.out.println(i);
+        }
+    
+    // using while loop-
+    // int i=0;
+    // while(i<=10){
+    //  System.out.println(i);
+    //  i++;
+    // }
+    }
+}
