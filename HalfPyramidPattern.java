6467d8ae65592f6a7a39544b922c96cbd4b7b056
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-04-03 08:25:31 +0530

    Implemented half pyramid pattern using nested loops

---
+++ HalfPyramidPattern.java
@@ -0,0 +1,16 @@
+// Pattern Practice
+// Program to print a half pyramid pattern using nested loops
+public class HalfPyramidPattern {
+
+    public static void main(String[] args) {
+        int n=4;
+        // outer loop
+        for(int i=1;i<=n;i++){
+            // inner loop
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
