d36547df3c1ae2fefd50a8f6a75128f503eba719
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-04-03 07:36:16 +0530

    Added program to print solid rectangle pattern

---
+++ SolidRectanglePattern.java
@@ -0,0 +1,17 @@
+// Pattern Practice
+// Program to print a solid rectangle pattern using nested loops
+public class SolidRectanglePattern {
+
+    public static void main(String[] args) {
+        int n=4;
+        int m=5;
+        // outer loop
+        for(int i=0;i<n;i++){
+            // inner loop
+            for(int j=0;j<m;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
